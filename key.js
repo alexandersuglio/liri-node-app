182ca781c644eac20ae4d5e991b6af255cbb1250
Alex <alexandersuglio@gmail.com>
2017-04-20 17:38:30 -0400

    added liri.js file

---
+++ key.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'tytN1oRR63ICErVT7Ys9J7NBP',
+  consumer_secret: 'YGwgxzYU7cRBDcIcS1XmkVrKaXqz4aWWSxaokptmehVFdlzrGD',
+  access_token_key: '	854779100719980544-67f4Vbhjhyw2Hf9LoH3a2XlFFXhA3dr',
+  access_token_secret: 'k5NBUjrXuWOkYvqYOj18HtswFcq5yrUQgkmNydnwoSQCC',
+}
+
---
+++ liri.js
@@ -0,0 +1,73 @@
+
+// var twitter = require("twitter");
+// var request = require("Request");
+
+
+var fs = require("fs");
+
+// shows the contents of the array, [3] & [4]
+// console.log(process.argv);
+
+// console.log("Welcome to the spotify-this app! To begin enter the name of the song you want more info about below!");
+var spotify = require("spotify");
+var liriArgument = process.argv[2];
+switch (liriArgument) {
+
+    case "spotify-this":
+        spotifyMe();
+        break;
+
+       
+        //make room for future cases here
+        // case "ruby-this":
+        //     rubyThis();
+        //     break;
+};
+
+
+function spotifyMe() {
+    // console.log("I love spotify");
+
+    var songEnter = process.argv[3];
+    //if argument is left blank, it's undefined, reassign it's value to 'the sign'
+    if (songEnter === undefined) {
+        songEnter = "the sign";
+        //concatenate 'the sign'
+        spotify.search({ type: 'track', query: songEnter }, function(err, data) {
+            if (data) {
+                console.log("Song Name: " + data.tracks.items[3].name);
+                console.log("Artist's Name: " + data.tracks.items[3].artists[0].name);
+                console.log("Album Name: " + data.tracks.items[3].album.name);
+                console.log("Song Link: " + data.tracks.items[3].external_urls.spotify);
+            }
+        });
+
+    } else {
+        spotify.search({ type: 'track', query: songEnter }, function(err, data) {
+
+            if (data) {
+                console.log("Song Name: " + data.tracks.items[0].name);
+                console.log("Artist's name: " + data.tracks.items[0].album.artists[0].name);
+                console.log("Album Name: " + data.tracks.items[0].album.name);
+                console.log("Song Link : https://play.spotify.com/track/" + data.tracks.items[0].id + "?play=true&utm_source=open.spotify.com&utm_medium=open");
+            }
+
+
+
+        });
+    };
+
+};
+
+
+//Future functions go here
+// function rubyThis() {
+//     var dogFriend = process.argv[3];
+//     console.log(dogFriend);
+//     // console.log("Ruby loves you");
+// };
+
+
+
+
+
